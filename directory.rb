94d1df95f2f0bcb802404651c53ce0acfe7cb88f
Jose <jcopoviking@outlook.com>
2016-06-27 14:38:13 +0100

    Addtion of 'save_students' method

--- directory.rb
+++ directory.rb
@@ -1,35 +1,11 @@
+@students = [] # and empty array accessible to all methods
 
-def interactive_menu
-  students = []
-  loop do
-    # 1.  print the menu and ask the user what to do
-    puts "1.  Input the students"
-    puts "2.  Show the students"
-    puts "9.  Exit"  # 9 because we will be asking more questions
-    # 2.  read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to exit
-    else
-      puts "I don't understand what you meant, try again"
-    end
-  end
-end
-
-
-
-  def input_students
+def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit the return twice"
+  # get the first name
   name = gets.chomp
+  # while the name is not empty, repeat this code
   puts "Now please enter the student's cohort month"
   cohort = gets.chomp
   #create an empty array
@@ -39,8 +15,8 @@ end
   #while the name is empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort, hobbies: :hobby, height: :tall, country: :nation}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, hobbies: :hobby, height: :tall, country: :nation}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     puts "Please enter the names of the students"
     puts "To finish, just hit the return twice"
@@ -48,8 +24,39 @@ end
     puts "Now please enter the student's cohort month"
     cohort = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+    # 1.  print the menu and ask the user what to do
+      puts "1.  Input the students"
+      puts "2.  Show the students"
+      puts "9.  Exit"  # 9 because we will be asking more question
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit # this will cause the program to exit
+    else
+      puts "I don't understand what you meant, try again"
+  end
 end
 
 def print_header
@@ -57,9 +64,8 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-
-    students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
       student.each do |name, value|
         while value.downcase[0] == 'v' && value.length < 12
         puts"#{index+1}.#{student[:name]} (#{student[:cohort]} cohort)".center(80)
@@ -72,15 +78,26 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  if names.count > 1
-    puts "overall, we have #{names.count} great students."
+def print_footer
+  if @students.count > 1
+    puts "Overall, we have #{@students.count} great students."
   else
-    puts "overall, we have #{names.count} great student"
+    puts "Overall, we have #{@students.count} great student"
   end
 end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
 #nothing happens untill we call the methods
 interactive_menu
-print_header
-print(students)
-print_footer(students)
