8a1076c0d6e213e6a2500a67c6abdb4f1fb2a78c
Jose <jcopoviking@outlook.com>
2016-06-27 19:06:05 +0100

    .gitignore file addtion. addtion of load_students method to directory.rb

--- directory.rb
+++ directory.rb
@@ -38,6 +38,7 @@ def print_menu
       puts "1.  Input the students"
       puts "2.  Show the students"
       puts "3.  Save students list to students.csv"
+      puts "4.  Load the list from students.csv"
       puts "9.  Exit"  # 9 because we will be asking more question
 end
 
@@ -55,6 +56,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit # this will cause the program to exit
     else
@@ -99,6 +102,15 @@ def save_students
     file.puts csv_line
   end
   file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+      @students << {name:  name, cohort: cohort.to_sym}
+  end
+    file.close
 end
 
 
